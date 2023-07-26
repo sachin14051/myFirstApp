0f0cea29a23606c6ba90f5a979a996fd1027c9f2
sachin <sachin14051@gmail.com>
2023-07-26 23:37:28 +0530

    my first angular project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { homeComponent } from 'src/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    homeComponent
   ],
   imports: [
     BrowserModule
